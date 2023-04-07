75b028e913e3e0cde3b62076b2e75edf38a77b18
dnjswndtjq <dnjswndtjq@naver.com>
2023-04-07 15:29:23 +0900

    [chore] firebase service worker messaging version update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,23 @@
-importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+// importScripts(
+//   'https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js',
+// );
 
-firebase.initializeApp({
+// firebase.initializeApp({
+//   apiKey: 'AIzaSyD9UMmEqiCoTo-GHPWrvgynK88HKefTeLU',
+//   authDomain: 'peeps-store-22f24.firebaseapp.com',
+//   projectId: 'peeps-store-22f24',
+//   storageBucket: 'peeps-store-22f24.appspot.com',
+//   messagingSenderId: '260208879136',
+//   appId: '1:260208879136:web:30aaea5c3c42377c04fe45',
+//   measurementId: 'G-K6JLR9GG38',
+// });
+// const messaging = firebase.messaging();
+
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging/sw';
+
+const firebaseApp = initializeApp({
   apiKey: 'AIzaSyD9UMmEqiCoTo-GHPWrvgynK88HKefTeLU',
   authDomain: 'peeps-store-22f24.firebaseapp.com',
   projectId: 'peeps-store-22f24',
@@ -10,4 +26,5 @@ firebase.initializeApp({
   appId: '1:260208879136:web:30aaea5c3c42377c04fe45',
   measurementId: 'G-K6JLR9GG38',
 });
-const messaging = firebase.messaging();
+
+const messaging = getMessaging(firebaseApp);
